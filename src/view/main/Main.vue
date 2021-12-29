f5be20e7706d3b4116f50bd57ec5da6a55a27980
lleeooo <584395720@qq.com>
2021-12-29 23:31:25 +0800

    feat: init admin-base

---
+++ src/view/main/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>main{{ $store.state.name }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+  mutations: {},
+  getters: {},
+  actions: {}
+})
+</script>
+
+<style scoped></style>
